68ddcfc8c5097b045127b39fd9800877af8aa406
cfm <codefrommars@gmail.com>
2019-09-02 22:17:32 +0200

    112 done

--- uva/112.cpp
+++ uva/112.cpp
@@ -1,62 +1,49 @@
 #include <cstdio>
 #include <cctype>
 
-bool next(int* value){	
-	char c = getchar();
-	while( c == ' ' || c == '\n' || c == '\t')
-		c = getchar();
-	
-	//non whitespace
-	if( c == '(' || c == ')' )
-		return false;
-	
-	int sign = 1;
-	
-	//is it a sign?
-	if ( c == '-' ){
-		sign = -1;
-		c = getchar();
-	}
-	int num = 0;
-	//it's an integer.	
-	while( isdigit(c) ){		
-		num = num * 10 + (c - '0');	
-		c = getchar();		
-	}
-	*value = sign * num;
-	//std::printf("Read value: %i ungetting: '%c'(%i)\n", *value, c, c);
-	ungetc(c, stdin);
-	
-	return true;
-}
-int find_sum(int sum, int k, int* found){
+bool found;
 
-	int value = 0;	
-	next(&value);	//"l par	
-	if( !next(&value) )		
-		return 0;
+void skip() {
+	char t;
+	std::scanf(" %c", &t);
+}
+
+bool num(int* value) {
+	int read = std::scanf(" %i", value);
+	return ( read > 0 );
+}
+bool find_sum(int sum, int k){
+
+	int value = 0;
+	
+	skip(); //l par	
+	
+	if( !num(&value) ){
+		skip(); //r par
+		return false; // empty tree
+	}
 	
 	int node_val = value + k;
 	
-	int lval = find_sum(sum, node_val, found);	
-	int rval = find_sum(sum, node_val, found);
+	bool lval = find_sum(sum, node_val);	
+	bool rval = find_sum(sum, node_val);
 	
-	if( lval == 0 && rval == 0 )
+	if( !lval  && !rval )
 		if( node_val == sum )
-			*found = 1;
+			found = true;
 
-	next(&value);
+	skip();
 
-	return value;
+	return true;
 }
 
 int main(){
 	int sum;
-	int found;
+	
 	while( std::scanf("%i", &sum) != EOF ){		
-		found = 0;
-		find_sum(sum, 0, &found);
-		if( found == 1 )
+		found = false;
+		find_sum(sum, 0);
+		if( found )
 			std::printf("yes\n");
 		else
 			std::printf("no\n");
