dfbdb963ed67f61ddc78f14fa726868a2e9877e1
cfm <codefrommars@gmail.com>
2020-12-02 14:21:20 +0100

    problem 2

---
+++ aoc2020/2.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+bool IsValid1(int min, int max, char c, char* str){
+    int count = 0;
+    for(int i = 0; str[i] != '\0'; i++){
+        if( str[i] == c )
+            count++;
+        if( count > max )
+            return false;
+    }
+
+    return count >= min;
+}
+
+
+
+int part1() {
+
+    int min, max;
+    char c;
+    char str[100];
+    int valid = 0;
+    while( scanf("%i-%i %c: %s\n", &min, &max, &c, &str[0]) > 0 ) {
+        if(IsValid1(min, max, c, str))
+            valid ++;
+    }
+
+    std::cout << valid << std::endl;
+
+    return 0;
+}
+
+bool IsValid2(int p1, int p2, char c, char* str){
+    return (str[p1 - 1] == c) ^ (str[p2 - 1] == c);
+}
+
+int part2() {
+
+    int min, max;
+    char c;
+    char str[100];
+    int valid = 0;
+    
+    while( scanf("%i-%i %c: %s\n", &min, &max, &c, &str[0]) > 0 ) {
+        if(IsValid2(min, max, c, str))
+            valid ++;
+    }
+
+    std::cout << valid << std::endl;
+
+    return 0;
+}
+
+int main() {
+    return part2();
+}
