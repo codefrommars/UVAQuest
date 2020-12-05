251c37e42abb499062e6e0ee55c6edcc7911128d
codefrommars <codefrommars@gmail.com>
2020-12-05 19:29:48 +0100

    aoc2020 05 recursive

--- aoc2020/05.cpp
+++ aoc2020/05.cpp
@@ -1,29 +1,21 @@
 #include <iostream>
+#include <algorithm>
 
-int decode( const char* c, char cOne, int mult ){
-    if( mult == 0 )
-        return 0;
+int seatID( const char* c , int id){
+    if( *c == 0 )
+        return id;
 
-    int val = ( *c == cOne ) * mult; 
-    return decode(c + 1, cOne, mult >> 1 ) + val;
-}
-
-int seatID(const char* line){
-    int row = decode(line, 'B', 64);
-    int col = decode(&line[7], 'R', 4);
-    return row * 8 + col;
+    int bit = ( *c == 'B' || *c == 'R' ) ; 
+    return seatID(c + 1, 2 * id + bit ) ;
 }
 
 void part1() {
     char line[16];
     int maxSeat = 0;
 
-    while( std::cin >> line ){
-        int id = seatID(line);
-        if( id > maxSeat )
-            maxSeat = id;
-    }
-
+    while( std::cin >> line )
+        maxSeat = std::max(maxSeat, seatID(line, 0) );
+    
     std::cout << maxSeat << std::endl;
 }
 
@@ -31,10 +23,8 @@ void part2() {
     bool seats[128*8] = {false};
     char line[16];
 
-    while( std::cin >> line ){
-        int id = seatID(line);
-        seats[id] = true;
-    }
+    while( std::cin >> line )
+        seats[seatID(line, 0)] = true;
 
     for( int i = 1; i < 1023; i++ ){
         if( seats[i - 1] && !seats[i] && seats[i + 1] ){
@@ -45,6 +35,6 @@ void part2() {
 }
 
 int main() {
-    part2();
+    part1();
     return 0;
 }
