bd7a1c6f545ecfe3746359eee26d7432d9a9deb6
cfm <cfm@cfm.com>
2020-12-08 12:33:14 +0100

    aoc2020 08

---
+++ aoc2020/08.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+#include <utility>
+
+using Instruction = std::pair<char, int>;
+
+int exec(const Instruction& inst, int* acum){
+    if( inst.first == 'j' )
+        return inst.second;
+    
+    if( inst.first == 'a' )
+        *acum += inst.second;
+    return 1;
+}
+
+std::vector<Instruction> load(){
+    std::string mnemonic;
+    int param;
+    std::vector<Instruction> program;
+    for( std::string line;  std::cin >> mnemonic >> param ; )
+        program.push_back( Instruction(mnemonic[0], param) );
+    return program;
+}
+
+// ---- Part 1 ----
+void part1() {
+    std::vector<Instruction> program = load();
+    std::set<int> executed;
+    int acum = 0;
+    for( int pc = 0; executed.count(pc) == 0; pc += exec(program[pc], &acum) )
+        executed.insert(pc);
+    
+    std::cout << acum << std::endl;
+}
+
+// ---- Part 2 ----
+bool fixes(std::vector<Instruction> program, int index){
+    program[index].first = (program[index].first == 'n') ? 'j' : 'n'; //Patch the program
+    std::set<int> executed;
+    int acum = 0;
+    for(int pc = 0; pc < program.size(); pc += exec(program[pc], &acum) ){
+        if( executed.count(pc) != 0 )
+            return false;
+        executed.insert(pc);
+    }
+    std::cout << acum << std::endl;
+    return true;
+}
+
+void part2(){
+    std::vector<Instruction> program = load();
+    int acum = 0;
+    for(int pc = 0; pc < program.size();  pc += exec(program[pc], &acum) ){
+        if( program[pc].first != 'a' )
+            if( fixes(program, pc) )
+                return;
+    }
+}
+
+int main() {
+    part2();
+    return 0;
+}
